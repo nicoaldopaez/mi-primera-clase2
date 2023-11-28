b348d08a75da2535a526a9a03f38a1d9f0c0888d
Nicolas <nicoaldopaez@gmail.com>
2023-11-28 12:39:16 +0100

    Añadidos métodos getDetalles e imprimirEstadoç

--- Mascota.java
+++ Mascota.java
@@ -2,32 +2,60 @@ public class Mascota{
     private String nombre;
     private int kilos;
     private boolean dormido;
-    
+
     public Mascota(String nomMascota, int kgs){
         nombre = nomMascota;
         kilos = kgs;
         dormido = false;
     }
+
     public String getNombre(){
         return nombre;
     }
+
     public int getKilos(){
         return kilos;
     }
+
     public boolean getEstado(){
         return dormido;
     }
+
     public void setNombre(String nuevoNombre){
         nombre = nuevoNombre;
     }
+
     public void setKilos(int nuevoPeso){
         kilos = nuevoPeso;
     }
+
     public void setEstado(){
         dormido = true;
     }
 
-    
+    public void imprimirEstado(){
+        String estado = "";
+        if(dormido != true){
+            estado = "no";
+        }
+        else{
+            estado = "si";
+        }
+        System.out.println("Nombre:"+nombre+"|"+"Peso:"+kilos+"|"+"Dormido:"+estado);
+    }
+
+    public String getDetalles(){
+        String estado = "";
+        if(dormido != true){
+            estado = "no";
+        }
+        else{
+            estado = "si";
+        }
+        String detalles = "Nombre:"+nombre+"|"+"Peso:"+kilos+"|"+"Dormido:"+estado;
+        
+        return detalles;
+    }
 
 
 
